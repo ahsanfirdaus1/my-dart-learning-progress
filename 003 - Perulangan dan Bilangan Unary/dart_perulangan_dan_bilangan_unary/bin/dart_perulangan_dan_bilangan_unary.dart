bb7ba4229f9d73df1dea7d2cb1d420aa4e3e5dab
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-23 10:40:27 +0700

    Membuat program dart perulangan dan bilangan unary

---
+++ 003 - Perulangan dan Bilangan Unary/dart_perulangan_dan_bilangan_unary/bin/dart_perulangan_dan_bilangan_unary.dart
@@ -0,0 +1,5 @@
+import 'package:dart_perulangan_dan_bilangan_unary/dart_perulangan_dan_bilangan_unary.dart' as dart_perulangan_dan_bilangan_unary;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_perulangan_dan_bilangan_unary.calculate()}!');
+}
