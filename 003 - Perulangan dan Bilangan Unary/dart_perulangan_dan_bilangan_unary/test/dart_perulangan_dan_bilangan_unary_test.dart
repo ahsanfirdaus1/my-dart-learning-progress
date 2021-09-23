93460f114168ec0880a9d344492e039d722730a6
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-23 10:52:18 +0700

    Menambahkan ke stage area sisa dari selain yang ada di file bin

--- 003 - Perulangan dan Bilangan Unary/dart_perulangan_dan_bilangan_unary/test/dart_perulangan_dan_bilangan_unary_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_perulangan_dan_bilangan_unary/dart_perulangan_dan_bilangan_unary.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
