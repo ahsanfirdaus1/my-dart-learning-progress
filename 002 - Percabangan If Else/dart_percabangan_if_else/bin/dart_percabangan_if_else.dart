042a36b8212619cbe4310da1b840222c773b34f6
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-23 10:17:31 +0700

    Membuat dart project percabangan dan mengganti sdk environemt ke minimal sama dengan 2.10.0

---
+++ 002 - Percabangan If Else/dart_percabangan_if_else/bin/dart_percabangan_if_else.dart
@@ -0,0 +1,5 @@
+import 'package:dart_percabangan_if_else/dart_percabangan_if_else.dart' as dart_percabangan_if_else;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_percabangan_if_else.calculate()}!');
+}
