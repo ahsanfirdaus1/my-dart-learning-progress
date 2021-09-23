ec8755faa161bd4fb9963f59015a0273bc534950
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-23 10:53:10 +0700

    Menambahkan sisanya

--- 002 - Percabangan If Else/dart_percabangan_if_else/test/dart_percabangan_if_else_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_percabangan_if_else/dart_percabangan_if_else.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
