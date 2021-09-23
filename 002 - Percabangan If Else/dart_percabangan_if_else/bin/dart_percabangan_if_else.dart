d4c310be90376ca226ac71314fbc994f4667edd5
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-23 10:36:05 +0700

    Membuat program percabangan dengan menggunakan switch case

--- 002 - Percabangan If Else/dart_percabangan_if_else/bin/dart_percabangan_if_else.dart
+++ 002 - Percabangan If Else/dart_percabangan_if_else/bin/dart_percabangan_if_else.dart
@@ -6,16 +6,26 @@ void main(List<String> arguments) {
   print("Angka: ");
   int Angka = int.tryParse(stdin.readLineSync());
   String Status ; 
-  if (Angka < 0) {
-    Status = 'Negatif';
-  } else if (Angka > 0) {
-    Status = 'Positif';
-  } else { 
-    Status = 'Nol'; 
+  // if (Angka < 0) {
+  //   Status = 'Negatif';
+  // } else if (Angka > 0) {
+  //   Status = 'Positif';
+  // } else { 
+  //   Status = 'Nol'; 
+  // }
+  switch (Angka){
+    case 0:
+      Status = 'Nol' ;
+      break;
+    case 1: 
+      Status = 'Satu' ;
+      break;
+    default: 
+      Status = 'Selain Nol dan Satu';
   }
   
   String Notif = 'Angka ' + Angka.toString() + ' merupakan bilangan ' + Status ;
   print(Notif);
   print('Program Selesai');
-  
+
 }
